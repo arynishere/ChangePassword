b210a2008a7dda3b2810a62af8f95c902c4f5b5e
Youngrey <95162220+arynishere@users.noreply.github.com>
2024-01-06 13:21:10 +0330

    Update chpass.sh

--- chpass.sh
+++ chpass.sh
@@ -2,4 +2,4 @@
 
 read -p "enter your user name : "username
 read -p "File name for your password incloud :"filename
-ansible-playbook -i hosts_file playbook.yml --extra-vars "username=$username filename=$filename"
+ansible-playbook -i $hostfile playbook.yml --extra-vars "username=$username filename=$filename hostname=$hostname"
